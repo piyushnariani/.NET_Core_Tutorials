853ccd529e39da600827d2213d9542b2009d0bf3
Piyush <piyushnariani09@gmail.com>
2020-06-28 02:55:02 -0400

    Login with JWT Auth

--- DatingApp.API/DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/DatingApp.API/Dtos/UserForLoginDto.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Dtos
 {
-    public class UserForRegisterDto
+    public class UserForLoginDto
     {
-        [Required]
         public string Username { get; set; }
-
-        [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "Your password must be between 4-8 characters")]
         public string Password { get; set; }
     }
 }
