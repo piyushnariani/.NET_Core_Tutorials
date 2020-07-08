5a4a28d441692719c50de65b64a249de400ba653
Piyush <piyushnariani09@gmail.com>
2020-07-08 15:17:21 -0400

    Auth0 Angular JWT

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {JwtHelperService} from '@auth0/angular-jwt';
 import {map} from 'rxjs/operators';
 
 @Injectable({
@@ -7,6 +8,7 @@ import {map} from 'rxjs/operators';
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
   constructor(private http: HttpClient) { }
 
   login(model: any) {
@@ -23,5 +25,10 @@ export class AuthService {
 
   register(model: any){
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
