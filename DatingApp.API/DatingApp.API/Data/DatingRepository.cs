96f1bfdf2de983596477257c0175aa68f5145514
piyushnariani <piyushnariani09@gmail.com>
2020-07-14 02:19:01 -0400

    Designed Dating Repository Service

---
+++ DatingApp.API/DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository: IDatingRepository
+    {
+        private readonly DataContext _db;
+
+        public DatingRepository(DataContext db)
+        {
+            _db = db;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _db.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _db.Remove(entity);
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _db.Users.Include(u => u.Photos).ToListAsync();
+            return users;
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _db.Users.Include(u => u.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+    }
+}
--- DatingApp.API/DatingApp.API/Startup.cs
+++ DatingApp.API/DatingApp.API/Startup.cs
@@ -38,6 +38,7 @@ namespace DatingApp.API
             services.AddControllers();
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -50,6 +51,7 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
